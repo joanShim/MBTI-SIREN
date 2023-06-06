211a7bb464a1f3074549d736ba93840442b71b49
joanShim <joanjeongahshim@gmail.com>
2023-06-07 00:58:51 +0900

    오류수정

--- js/share.js
+++ js/share.js
@@ -14,8 +14,8 @@ document.body.append(healthEl)
 // 모바일 브라우저 내장 공유 기능!
 async function startNativeShare() {
   await navigator.share({
-    title: '내 안에 숨어있는 직업캐 찾기!',
-    text: '누구나 찰떡인 직업이 있어요! 내 안에 숨어있는 직업캐를 찾아보세요!',
+    title: '사이렌: 불의 섬 나는 어떤 팀이 어울릴까?',
+    text: '사이렌: 불의 섬 나는 어떤 팀이 어울릴까?',
     url: location.href // 현재 페이지 주소!
   })
 }
